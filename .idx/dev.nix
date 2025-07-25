db322dc35eeb593d33b42edab3f3036f6936e4a5
geraldovercel <geraldovercel@gmail.com>
2025-07-25 03:36:41 +0000

    11

---
+++ .idx/dev.nix
@@ -0,0 +1,28 @@
+nix
+      {pkgs}: {
+        channel = "stable-24.05";
+        packages = [
+          pkgs.nodejs_20
+          pkgs.nextjs # Add this line to install Next.js
+        ];
+        idx.extensions = [
+
+        ];
+        idx.previews = {
+         previews = {
+           web = {
+             command = [
+               "npm"
+               "run"
+               "dev"
+               "--"
+               "--port"
+               "$PORT"
+               "--hostname"
+               "0.0.0.0"
+             ];
+             manager = "web";
+           };
+         };
+       };
+     }
